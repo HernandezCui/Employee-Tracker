fd5ada2ab2aed1b9f529fa0c4c8a61357c083c15
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-01 21:47:37 -0800

    finished prompt that will be displayed once logged in

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const inquirer = require('inquirer');
 const db = require('./db');
 const cTable = require('console.table');
 
+// function that will display onced logged in 
+function startApp() {
+    inquirer.
+    prompt({
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all routes',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add a employee',
+            'Update an employee role',
+        ],
+    })
+    .then((answer) => {
+       switch (answer.option) {
+        case 'View all departments':
+            viewAllDepartments();
+            break;
+       }
+    });
+}
