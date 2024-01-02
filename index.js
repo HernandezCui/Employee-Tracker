2ca9b0a93a638766126e945cb5fd7a881b546fbb
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-02 15:34:08 -0800

    adding all files back

---
+++ index.js
@@ -0,0 +1,207 @@
+// Dependencies 
+const inquirer = require('inquirer');
+const db = require('./db');
+const cTable = require('console.table');
+
+// function that will display onced logged in 
+function startApp() {
+    inquirer.
+    prompt({
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add a employee',
+            'Update an employee role',
+        ],
+    })
+    .then((answer) => {
+       switch (answer.option) {
+        case 'View all departments':
+            viewAllDepartments();
+            break;
+
+        case 'View all roles':
+            viewAllRoles();
+            break;
+
+        case 'View all employees':
+            viewAllEmployees();
+            break;
+
+        case 'Add a department':
+            addDepartment();
+            break;
+
+        case 'Add a role':
+            addRole();
+            break;
+        
+        case 'Add a employee':
+            addEmployee();
+            break;
+
+        case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+        
+        default: 
+            console.log('Goodbye!');
+
+            // Close database if needed
+            db.closeConnection();
+       }
+    });
+
+// function to handle viewing all departments and roles
+function viewAllDepartments() {
+    db.getAllDepartments()
+        .then((departments) => {
+            console.table(departments);
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+}
+
+function viewAllRoles() {
+    db.getAllRoles()
+        .then((roles) => {
+            console.table(roles);
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+}
+
+function viewAllEmployees() {
+    db.getAllEmployees()
+        .then((employees) => {
+            console.table(employees);
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+}
+
+
+// Call corresponding functions when adding departments and roles
+
+function addDepartment() {
+    inquirer
+    .prompt({
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the name of the department'
+    })
+    .then((answer) => {
+        db.addDepartment(answer.departmentName)
+        .then(() => {
+            console.log('Department added successfully');
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+    });
+}
+
+
+function addRole() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'Enter the name of the role'   
+        },
+        {
+            type: 'input',
+            name:'salaryTotal',
+            message: 'Enter the salary of the role'
+        },
+        {
+            type: 'input',
+            name: 'deptId',
+            message: 'Enter the department ID for the role'
+        }
+    ])
+    .then((answer) => {
+        db.addRole(answer.roleName, answer.salaryTotal, answer.deptId)
+        .then(() => {
+            console.log('Role added successfully');
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+    });
+}
+
+
+function addEmployee() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'employeeFirstName',
+            message: 'Enter the employees First Name'
+        },
+        {
+            type: 'input',
+            name: 'employeeLastName',
+            message: 'Enter the employees Last Name'
+        },
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'Enter the employees role id number'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter the managers id number'
+        }
+    ])
+    .then((answer) => {
+        db.getNewEmployee(answer.employeeFirstName, answer.employeeLastName, answer.roleId, answer.managerId)
+        .then(() => {
+            console.log('Employee added successfully');
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+    });
+}
+
+function updateEmployeeRole() {
+    db.getUpdatedEmployeeRole()
+        .then((employeeRole) => {
+            console.table(employeeRole);
+            startApp();
+        })
+        .catch((err) => {
+            console.error(err);
+            startApp();
+        });
+}
+
+
+
+
+}
