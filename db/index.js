2b81434d9aafa160336745361b29105e19b0b48a
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-02 19:22:30 -0800

    function to get all departments

--- db/index.js
+++ db/index.js
@@ -2,3 +2,25 @@
 const connection = require('./connection');
 
 
+// DB commands
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+    getAllDepartments() {
+        return this.connection.promise().query('SELECT * FROM department');
+    }
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
