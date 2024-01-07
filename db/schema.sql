8fd5852ea1fafab46b421a1b3e40c51eac18f818
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-06 17:17:43 -0800

    finished ny schema sql

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS company; 
+
+CREATE DATABASE company; 
+USE company;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles; 
+DROP TABLE IF EXISTS employee; 
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
