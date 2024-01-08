9aa3e32fb1d9297e5b5a80b42818cc19a0a13e10
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-08 00:57:54 -0800

    deleted a function on my connections.js

--- db/connection.js
+++ db/connection.js
@@ -8,12 +8,4 @@ const connection = mysql.createConnection({
     database: 'company', 
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:' + err.stack);
-        return;
-    }
-    console.log('Connected to MySQL as id' + connection.threadId);
-});
-
 module.exports = connection;
