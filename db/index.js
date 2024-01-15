b29b9e4cbbe34be87c1482982a3646b7124d60bb
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-14 22:28:02 -0800

    revised index.js for db

--- db/index.js
+++ db/index.js
@@ -11,45 +11,55 @@ class db {
         return this.connection.promise().query('SELECT * FROM department');
     }
     getAllRoles() {
-        return this.connection.promise().query('SELECT * FROM role');
+        return this.connection.promise().query(
+            'SELECT roles.id, roles.title, roles.salary, department.name AS department FROM roles LEFT JOIN department ON roles.department_id = department.id'
+            );
     }
     getAllEmployees() {
-        return this.connection.promise().query('SELECT * FROM employee');
+        return this.connection.promise().query(
+            `SELECT employee.id, CONCAT(employee.first_name, ' ' , employee.last_name) AS name, roles.title, department.name AS department, roles.salary, 
+            CONCAT(manager.first_name, ' ' , manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id`
+            );
     }
     addDepartment(departmentName) {
         return this.connection
         .promise()
         .query('INSERT INTO department (name) VALUES (?)', [departmentName]);
     }
-    addRole(roleName, salaryTotal, departmentId) {
+    addRole(roleTitle, salaryTotal, departmentId) {
         return this.connection
         .promise()
-        .query('INSERT INTO role (title, salary, departmentId) VALUES (?, ?, ?)', [roleName, salaryTotal, departmentId]);
+        .query('INSERT INTO role (title, salary, departmentId) VALUES (?, ?, ?)', [roleTitle, salaryTotal, departmentId]);
     }
-    addEmployee(employeeFirstName, employeeLastName, roleId, managerId) {
+    addEmployee(answer) {
         return this.connection
         .promise()
-        .query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [employeeFirstName, employeeLastName, roleId, managerId]); 
+        .query('INSERT INTO employee SET ?', answer); 
     }
-    updateEmployeeRole(employeeId, roleId) {
+    updateEmployeeRole(roleId, employeeId) {
         return this.connection
         .promise()
         .query('UPDATE employee SET role_id = ? WHERE id = ?', [roleId, employeeId]);
     }
-    updateEmployeesByManager(employeeId, managerId) {
+    updateEmployeesByManager(managerId, employeeId) {
         return this.connection
         .promise()
-        .query('UPDATE employee SET manager_id = ? WHERE id = ?', [managerId, employeeId]);
+        .query('UPDATE employee SET employee.manager_id = ? WHERE id = ?', [managerId, employeeId]);
     }
     viewEmployeesByManager(managerId) {
         return this.connection
         .promise()
-        .query('SELECT * FROM employee WHERE manager_id = ?', [managerId]);
+        .query(`SELECT employee.id, employee.manager_id, CONCAT(employee.first_name, ' ' , employee.last_name) 
+        AS name FROM employee LEFT JOIN roles on employee.role_id = roles.id WHERE manager_id = ?`, [managerId]);;
     }
     viewEmployeesByDepartment(departmentId) {
+        console.log("depId: ", departmentId);
         return this.connection
         .promise()
-        .query('SELECT * FROM employee WHERE department_id = ?', [departmentId]);
+        .query(`SELECT CONCAT(employee.first_name, ' ' , employee.last_name) AS name, department.name AS department
+        FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department on roles.department_id = department.id
+        WHERE department.id = ?`, [departmentId]);
     }
     deleteDepartment(departmentId) {
         return this.connection
@@ -59,17 +69,17 @@ class db {
     deleteRole(roleId) {
         return this.connection
         .promise()
-        .query('DELETE FROM role WHERE id = ?', [roleId]);
+        .query('DELETE FROM roles WHERE id = ?', [roleId]);
     }
     deleteEmployee(employeeId) {
         return this.connection
         .promise()
         .query('DELETE FROM employee WHERE id = ?', [employeeId]);
     }
-    budgetByDepartment(departmentId) {
+    budgetByDepartment() {
         return this.connection
         .promise()
-        .query('SELECT SUM(salary) as budget FROM role WHERE department_id = ?', [departmentId]);
+        .query('SELECT department.name AS department, department.id, SUM(salary) AS total_salary FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department on roles.department_id = department.id GROUP BY department.id');
     } 
     closeConnection() {
         this.connection.end();
