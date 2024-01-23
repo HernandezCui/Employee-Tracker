79c1ca8d72d7f2246c5812add7b2af3ec967c3c2
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-22 17:23:21 -0800

    revised index.js to get table to appear

--- index.js
+++ index.js
@@ -2,72 +2,35 @@ const inquirer = require("inquirer");
 const db = require("./db");
 require("console.table");
 
-async function mainMenu() {
-  const { menu } = await inquirer.prompt([
+const exit = () => {
+  console.log("Bye!");
+  process.exit(0);
+};
+
+const mainMenu = async () => {
+  const answer = await inquirer.prompt([
     {
       type: "list",
       name: "menu",
       message: "What would you like to do?",
       choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-        "Delete a department",
-        "Delete a role",
-        "Delete an employee",
-        "Exit",
+        { name: "View all departments", value: viewDepartments },
+        { name: "View all roles", value: viewRoles },
+        { name: "View all employees", value: viewEmployees },
+        { name: "Add a department", value: addDepartment },
+        { name: "Add a role", value: addRole },
+        { name: "Add an employee", value: addEmployee },
+        { name: "Update an employee role", value: updateEmployeeRole },
+        { name: "Delete a department", value: deleteDepartment },
+        { name: "Delete a role", value: deleteRole },
+        { name: "Delete an employee", value: deleteEmployee },
+        { name: "Exit", value: exit },
       ],
     },
   ]);
 
-  switch (menu) {
-    case "View all departments":
-      return viewDepartments();
-    case "View all roles":
-      return viewRoles();
-    case "View all employees":
-      return viewEmployees();
-    case "Add a department":
-      return addDepartment();
-    case "Add a role":
-      return addRole();
-    case "Add an employee":
-      return addEmployee();
-    case "Update an employee role":
-      return updateEmployeeRole();
-    case "Delete a department":
-      return deleteDepartment();
-    case "Delete a role":
-      return deleteRole();
-    case "Delete an employee":
-      return deleteEmployee();
-    case "Exit":
-      console.log("Bye!");
-      process.exit();
-  }
-}
-
-async function viewDepartments() {
-  const departments = await db.findAllDepartments();
-  console.table(departments);
-  mainMenu();
-}
-
-async function viewRoles() {
-  const roles = await db.findAllRoles();
-  console.table(roles);
-  mainMenu();
-}
-
-async function viewEmployees() {
-  const employees = await db.findAllEmployees();
-  console.table(employees);
-  mainMenu();
-}
+  answer.menu();
+};
 
 async function viewDepartments() {
   try {
